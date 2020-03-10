098acfc382be72f05d504c6c0b6fc73400e94403
djskim27 <djs.kim27@gmail.com>
2020-03-10 01:41:11 -0400

    add react router-dom, axios, and bootstrap 4

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>This is the people app</h1>
-            </div>
+            <Router>
+                <div>
+                    <button className='btn btn-success'>Hello</button>
+                </div>
+            </Router>
         );
     }
 }
